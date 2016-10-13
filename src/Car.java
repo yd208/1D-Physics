ded0c3563aeb5f536b198ffc68ae501e1a616f37
yd208 <yadiel.graciano@yahoo.com>
2016-10-12 19:16:33 -0500

    Abstraction
    Got rid of getters, setters, and class variables. Added personal
    elasticity to each car.

--- src/Car.java
+++ src/Car.java
@@ -1,57 +1,41 @@
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Car {
-
-	private double xcor;
-	private double ycor;
-	private Color color;
-	private double mass;
-	private double velocity;
-	private double momentum;
-	private double accel;
-	private Vector direction = new Vector(0, Track.getGravity().getAngle());
+//This class is for cars that are solid colored and do
+//not display mass visually
+public class Car extends Entity {
 	
 	public Car(){}
 	
 	public Car(double x, double y)
 	{
+		//Car direction
+		direction = new Vector(0, Track.getGravity().getAngle());
+		
+		//Car color
 		if((Math.random() * 10) > 5)
 			color = Color.BLUE;
 		else
 			color = Color.RED;
+		
+		//Car attributes
 		mass = 10;
-		velocity = 5;
+		velocity = 10;
 		momentum = 0;
 		accel = 1.5;
+		//Standard Elasticity is .7
+		elasticity = 1;
 		xcor = x;
 		ycor = y;
 	}
 	
-	public void drawCar(Graphics g)
+	@Override
+	public void drawEntity(Graphics g)
 	{
 		//g.setColor(Color.WHITE);
 		//g.drawRect((int)xcor, (int)ycor, 30, 80);
 		g.setColor(color);
 		g.fillRect((int)xcor, (int)ycor, 30, 80);
 	}
-	
-	//Getters for Car properties
-	public Color getColor(){return color;}
-	public double getXcor(){return xcor;}
-	public double getMass(){return mass;}
-	public double getVelocity(){return velocity;}
-	public double getMomentum(){return momentum;}
-	public double getAcceleration(){return accel;}
-	public Vector getDirection(){return direction;}
-	
-	//Setters for Car properties
-	public void setColor(Color c){color = c;}
-	public void setXcor(double x){xcor = x;}
-	public void setMass(double m){mass = m;}
-	public void setVelocity(double v){velocity = v;}
-	public void setMomentum(double m){momentum = m;}
-	public void setAcceleration(double a){accel = a;}
-	public void setDirection(Vector v){direction = v;}
 	
 }
