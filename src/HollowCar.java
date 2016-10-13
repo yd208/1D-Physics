cafb45b389d4b672ad9f01b2b317fe17f9cc4100
yd208 <yadiel.graciano@yahoo.com>
2016-10-12 19:19:25 -0500

    Second Type of Car
    Hollow car visually displays the mass of the car by creating a hole in
    the center of the car depending on the mass. Hence, the less mass they
    have the more "hollow" they will be.

---
+++ src/HollowCar.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+//This class allows for cars that visually display the mass of lighter cars
+//by creating holes in the center of the car
+public class HollowCar extends Entity {
+
+	public HollowCar(){}
+	
+	public HollowCar(double x, double y)
+	{
+		//Car direction
+		direction = new Vector(0, Track.getGravity().getAngle());
+		
+		//Car color
+		if((Math.random() * 10) > 5)
+			color = Color.BLUE;
+		else
+			color = Color.RED;
+		
+		//Car attributes
+		mass = 10;
+		velocity = 5;
+		momentum = 0;
+		accel = 1.5;
+		xcor = x;
+		ycor = y;
+	}
+	
+	@Override
+	public void drawEntity(Graphics g) {
+		
+	}
+
+}
